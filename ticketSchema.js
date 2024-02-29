f83319605c0caf905d2eace6815352dc52b60847
ByteSleuths <150552235+ByteSleuths@users.noreply.github.com>
2024-02-29 22:46:12 +0100

    Create ticketSchema.js

---
+++ ticketSchema.js
@@ -0,0 +1,10 @@
+class Ticket {
+  constructor(guild, category, creator, text) {
+    this.Guild = guild;
+    this.Category = category;
+    this.Creator = creator;
+    this.Text = text;
+  }
+}
+
+module.exports = Ticket;
