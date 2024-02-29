5803ce77ceb3881b76bc8c76212ad8d3e0f4e075
ByteSleuths <150552235+ByteSleuths@users.noreply.github.com>
2024-02-29 23:12:45 +0100

    Update ticketSchema.js

--- ticketSchema.js
+++ ticketSchema.js
@@ -1,10 +1,11 @@
 class Ticket {
-  constructor(guildId, categoryId, creatorId, text) {
-    this.guildId = guildId;
-    this.categoryId = categoryId;
-    this.creatorId = creatorId;
-    this.text = text;
-  }
+    constructor(guildId, categoryId, creatorId, text) {
+        // Initialiser les propriétés de la classe
+        this.guildId = guildId;
+        this.categoryId = categoryId;
+        this.creatorId = creatorId;
+        this.text = text;
+    }
 
 }
 
